6d2b5243cc8b71087b71fedb9f014dd0eca18610
atr <atr@ciklum.com>
2018-02-23 19:51:30 +0300

    Brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,61 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let checkedArr = str.split("");
+    var a = 0;
+
+    function findCurrentBracketsPair(curVal) {
+        return bracketsConfig.find(function (element) {
+            if (element[0] == curVal || element[1] == curVal) {
+                return true;
+            }
+
+            return false;
+        });
+    }
+
+    function findCloseIndex(array, index, openTagCounter) {
+        let resultIndex = 0;
+        let curVal = array[index];
+
+        let curentBracketsPair = findCurrentBracketsPair(curVal);
+
+        if (!curentBracketsPair) {
+            return false;
+        }
+
+        //isCloseTag
+        if (curVal === curentBracketsPair[1] && (curentBracketsPair[1] !== curentBracketsPair[0] || openTagCounter[curVal] === 1)) {
+            if (curentBracketsPair[1] === curentBracketsPair[0]) {
+                openTagCounter[curVal] = 0;
+            }
+            return index;
+        }
+
+        //isOpenTag
+        if (curVal === curentBracketsPair[0]) {
+            if (curentBracketsPair[1] === curentBracketsPair[0]) {
+                openTagCounter[curVal] = 1;
+            }
+
+            resultIndex = findCloseIndex(array, index + 1, openTagCounter);
+            if (array[resultIndex] !== curentBracketsPair[1]) {
+                throw new Error('OOPS!');
+            }
+        }
+
+        if (resultIndex < array.length - 1) {
+            return findCloseIndex(array, resultIndex + 1, openTagCounter);
+        } else {
+            return array.length;
+        }
+    }
+
+    try {
+        let result = findCloseIndex(checkedArr, 0, {});
+        return result === checkedArr.length;
+    }
+    catch (e) {
+        return false;
+    }
+
+    return true;
 }
